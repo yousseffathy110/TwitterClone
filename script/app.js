9facb56a1f7d47081a310db5edbcbf4ca16ac068
yousef fathy <yf1102003@gmail.com>
2023-12-02 09:29:55 +0200

    Fix : fixing some bugs

--- script/app.js
+++ script/app.js
@@ -8,5 +8,14 @@ function openNewPage() {
     window.open("./home-page.html", "_blank");
   }
 
-//   alert(`Hello ${input} !`);
+  //   alert(`Hello ${input} !`);
+}
+
+function validiate() {
+  let Sinput = document.getElementById("userInput").value;
+  if (Sinput == "") {
+    alert("Please fill the input field before continuing.");
+  } else {
+    window.open("./home-page.html", "_blank");
+  }
 }
