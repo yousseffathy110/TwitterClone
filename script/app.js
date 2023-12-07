15815361b719b5bd5345e3c5dcd359a60d278de1
yousef fathy <yf1102003@gmail.com>
2023-12-07 16:39:52 +0200

    Done !!

--- script/app.js
+++ script/app.js
@@ -19,3 +19,14 @@ function validate() {
     window.open("./home-page.html", "_blank");
   }
 }
+
+document.getElementById("signupButton").addEventListener("click", function () {
+  var firstName = document.getElementById("fnameInput").value;
+  var lastName = document.getElementById("lnameInput").value;
+  var listItem = document.createElement("li");
+  listItem.textContent = `New user: ${firstName} ${lastName}`;
+  var userList = document.getElementById("userList");
+  userList.appendChild(listItem);
+  document.getElementById("fnameInput").value = "";
+  document.getElementById("lnameInput").value = "";
+});
